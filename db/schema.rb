4e7ad0eec96b37b9d014c8e498662935821be572
Devin Balsimo <dbalsimo@gmail.com>
2020-03-27 01:58:21 +0000

    Added contact create action to save to db. used db:drop, db:reset, and db:migrate to reset the db since the code was not working properly.

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,11 @@
 ActiveRecord::Schema.define(version: 20200324051535) do
 
   create_table "contacts", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.text     "comments"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
