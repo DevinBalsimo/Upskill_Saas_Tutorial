770aa8e89b1cb57b6484a1e18fceddb8bba70606
Devin Balsimo <dbalsimo@gmail.com>
2020-04-14 23:03:06 +0000

    Added plan vars and params to home page.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,10 @@
 class PagesController < ApplicationController
+    #GET Request for / (our homepage)
     def home
+        #Basic and pro plans are stored in the database, we can associate or
+        #find these plans by their ID number using the following command.
+        @basic_plan = Plan.find(1)
+        @pro_plan = Plan.find(2)
     end
 
     def about
