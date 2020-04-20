a50a05c21763f445ca3a578ce6e91bee62eb5483
Devin Balsimo <dbalsimo@gmail.com>
2020-04-20 01:55:53 +0000

    Got the DB schema file updated properly.

--- db/schema.rb
+++ db/schema.rb
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20200420012529) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.integer  "plan_id"
+    t.string   "stripe_customer_token"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
