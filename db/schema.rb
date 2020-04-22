92bc3f0642fd10c9f92841799f0677b5f0bf4ac3
Devin Balsimo <dbalsimo@gmail.com>
2020-04-22 18:37:20 +0000

    Added user profiles table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200420012529) do
+ActiveRecord::Schema.define(version: 20200422183634) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
@@ -25,6 +25,9 @@ ActiveRecord::Schema.define(version: 20200420012529) do
     t.decimal  "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "profiles", force: :cascade do |t|
   end
 
   create_table "users", force: :cascade do |t|
